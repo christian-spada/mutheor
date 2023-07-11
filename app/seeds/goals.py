9311d673e9a2c2e3f3a20a74c9bf7377c27b67fc
Christian Spada <spada17@gmail.com>
2023-07-11 19:06:50 -0400

    add goals seed data

---
+++ app/seeds/goals.py
@@ -0,0 +1,44 @@
+from app.models import db, Goal, environment, SCHEMA
+from sqlalchemy.sql import text
+from datetime import date
+
+
+def seed_goals():
+    goal1 = Goal(
+        user_id=1,
+        instrument_type=1,
+        description="Master paradiddles",
+        target_date=date(2023, 10, 12),
+    )
+    goal2 = Goal(
+        user_id=2,
+        instrument_type=2,
+        description="Get economy picking 16th notes up to 90 BPM",
+        target_date=date(2024, 2, 12),
+    )
+    goal3 = Goal(
+        user_id=3,
+        instrument_type=3,
+        description="Master spread triad voicings",
+        target_date=date(2024, 12, 11),
+    )
+
+    db.session.add(goal1)
+    db.session.add(goal2)
+    db.session.add(goal3)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_goals():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.goals RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM goals"))
+
+    db.session.commit()
