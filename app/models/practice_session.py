d69671e5efa6bc7620980db6c24a124a11ea8076
Christian Spada <spada17@gmail.com>
2023-07-11 16:09:18 -0400

    add PracticeSession model

---
+++ app/models/practice_session.py
@@ -0,0 +1,43 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.sql import func
+
+
+class PracticeSession(db.Model):
+    __tablename__ = "practice_sessions"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False
+    )
+    instrument_id = db.Column(
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod("instruments.id")),
+        nullable=False,
+    )
+    duration = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    notes = db.Column(db.String(255), nullable=False)
+    area_of_focus = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(
+        db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+    def to_dict(self, timestamps=False):
+        dct = {
+            "id": self.id,
+            "userId": self.user_id,
+            "instrumentId": self.instrument_id,
+            "duration": self.duration,
+            "date": self.date,
+            "notes": self.notes,
+            "areaOfFocus": self.area_of_focus,
+        }
+        if timestamps:
+            dct["createdAt"] = self.created_at
+            dct["updatedAt"] = self.updated_at
+
+        return dct
