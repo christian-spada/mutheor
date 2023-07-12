570314133a8030e34446dbfef82d20d6446bc314
Christian Spada <spada17@gmail.com>
2023-07-12 15:30:55 -0400

    add create practice session form

---
+++ app/forms/practice_sessions/create_practice_session.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SelectField, IntegerField, DateTimeField
+from wtforms.validators import DataRequired, ValidationError
+
+area_of_focus_choices = [
+    ("Chords"),
+    ("Scales"),
+    ("Technique"),
+    ("Theory"),
+    ("Rhythm"),
+    ("Repertoire"),
+]
+
+
+class CreatePracticeSession(FlaskForm):
+    user_id = IntegerField("user_id", validators=[DataRequired()])
+    instrument_id = IntegerField("instrument_id", validators=[DataRequired()])
+    duration = IntegerField("duration", validators=[DataRequired()])
+    date = DateTimeField("date", validators=[DataRequired()])
+    notes = TextAreaField("date", validators=[DataRequired()])
+    area_of_focus = SelectField(
+        "area_of_focus", choices=area_of_focus_choices, validators=[DataRequired()]
+    )
