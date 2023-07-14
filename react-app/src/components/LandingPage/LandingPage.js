5e885bb1f5ef09dc39c35c21ebc596aff3224f91
Christian Spada <spada17@gmail.com>
2023-07-14 17:11:53 -0400

    work on landing page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,9 +1,26 @@
+import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
+
+const NavBar = () => {
+  const history = useHistory();
+  return (
+    <nav className="landing-page__navbar">
+      <div className="landing-page__logo-container">
+        <div className="landing-page__logo"></div>
+        <span className="landing-page__app-title">Mutheor</span>
+      </div>
+      <div className="landing-page__action-btn-container">
+        <button onClick={() => history.push('/login')}>Login</button>
+        <button onClick={() => history.push('/signup')}>Signup</button>
+      </div>
+    </nav>
+  );
+};
 
 const LandingPage = () => {
   return (
-    <div className="divvy">
-      <h1> LandingPage </h1>
+    <div className="landing-page">
+      <NavBar />
     </div>
   );
 };
---
+++ react-app/src/components/LoginFormPage/LoginFormPage.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { login } from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import './LoginForm.css';
+
+const LoginFormPage = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) {
+    history.push('/');
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const data = await dispatch(login(email, password));
+    if (data) {
+      setErrors(data);
+    }
+  };
+
+  return (
+    <>
+      <div className="login-page__sidebar"></div>
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <label>
+          Email
+          <input value={email} onChange={e => setEmail(e.target.value)} required />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Log In</button>
+      </form>
+    </>
+  );
+};
+
+export default LoginFormPage;
