785906dd306680249cbc9902abf835ac5da42cee
Christian Spada <spada17@gmail.com>
2023-07-15 16:36:18 -0400

    add hamburger component + sidebar functionality

--- react-app/src/components/UserDashboard/UserDashboard.js
+++ react-app/src/components/UserDashboard/UserDashboard.js
@@ -1,6 +1,41 @@
+import { useSelector } from 'react-redux';
+import ProfileButton from '../Navigation/ProfileButton';
 import './UserDashboard.css';
+import { useState } from 'react';
+
+const HamburgerMenu = ({ showSideBar, setShowSidebar }) => {
+  const handleClick = e => {
+    setShowSidebar(!showSideBar);
+  };
+
+  const menuBarClass = `hamburger-menu-bar ${showSideBar ? 'open ' : ''}`;
+
+  return (
+    <div className="hamburger-menu" onClick={handleClick}>
+      <div className={menuBarClass + 'bar-1'}></div>
+      <div className={menuBarClass + 'bar-2'}></div>
+      <div className={menuBarClass + 'bar-3'}></div>
+    </div>
+  );
+};
 
 const UserDashboard = () => {
-  return <h1> UserDashboard </h1>;
+  const user = useSelector(state => state.session.user);
+  const [showSideBar, setShowSidebar] = useState(false);
+
+  return (
+    <>
+      <header className="user-dashboard__header">
+        <HamburgerMenu showSideBar={showSideBar} setShowSidebar={setShowSidebar} />
+        <div className="user-dashboard__profile-container">
+          <ProfileButton user={user} />
+        </div>
+      </header>
+      <main>
+        <aside className={`user-dashboard__sidebar ${showSideBar ? 'show' : ''}`}></aside>
+        {showSideBar && <div className="overlay"></div>}
+      </main>
+    </>
+  );
 };
 export default UserDashboard;
