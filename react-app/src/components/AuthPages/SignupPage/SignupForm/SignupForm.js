e9f266ead59f2579e09efd101ec16deecd271c27
Christian Spada <spada17@gmail.com>
2023-07-14 20:03:45 -0400

    work on signup page

---
+++ react-app/src/components/AuthPages/SignupPage/SignupForm/SignupForm.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { signUp } from '../../../store/session';
+import './SignupForm.css';
+
+export const SignupForm = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      const data = await dispatch(signUp(username, email, password));
+      if (data) {
+        setErrors(data);
+      }
+    } else {
+      setErrors(['Confirm Password field must be the same as the Password field']);
+    }
+  };
+  return (
+    <div className="form-wrapper">
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <label>
+          Email
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)} required />
+        </label>
+        <label>
+          Username
+          <input
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Confirm Password
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
