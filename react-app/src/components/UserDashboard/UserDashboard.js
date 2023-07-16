65fa34e618ab7b0328a1afcf6909a63cea53839e
Christian Spada <spada17@gmail.com>
2023-07-15 22:46:27 -0400

    work on user dash board

--- react-app/src/components/UserDashboard/UserDashboard.js
+++ react-app/src/components/UserDashboard/UserDashboard.js
@@ -1,28 +1,45 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from '../ProfileButton/ProfileButton';
 import './UserDashboard.css';
 import { useEffect, useRef, useState } from 'react';
-
-const HamburgerMenu = ({ showSideBar, setShowSidebar }) => {
-  const handleClick = e => {
-    setShowSidebar(!showSideBar);
-  };
-
-  const menuBarClass = `hamburger-menu-bar ${showSideBar ? 'open ' : ''}`;
-
-  return (
-    <div className="hamburger-menu" onClick={handleClick}>
-      <div className={menuBarClass + 'bar-1'}></div>
-      <div className={menuBarClass + 'bar-2'}></div>
-      <div className={menuBarClass + 'bar-3'}></div>
-    </div>
-  );
-};
+import HamburgerMenu from './HamburgerMenu/HamburgerMenu';
+import UserInfoCard from './UserInfoCard/UserInfoCard';
+import PracticeSessionCard from './PracticeSessionCard/PracticeSessionCard';
+import InstrumentCard from './InstrumentCard/InstrumentCard';
+import GoalCard from './GoalCard/GoalCard';
+import './UserDashboard.css';
+import { logger } from '../../utils/helpers';
+import { thunkGetAllPracticeSessions } from '../../store/practiceSessions';
+import { thunkGetAllInstruments } from '../../store/instruments';
+import { thunkGetAllGoals } from '../../store/goals';
 
 const UserDashboard = () => {
+  const dispatch = useDispatch();
   const user = useSelector(state => state.session.user);
+  const practiceSessions = useSelector(state =>
+    Object.values(state.practiceSession.allPracticeSessions)
+  );
+  const instruments = useSelector(state => Object.values(state.instrument.allInstruments));
+  const goals = useSelector(state => Object.values(state.goal.allGoals));
   const [showSideBar, setShowSidebar] = useState(false);
+  const [contentView, setContentView] = useState('Practice Sessions');
   const sidebar = useRef();
+
+  const handlePracticeSessionClick = async e => {
+    setContentView('Practice Sessions');
+
+    dispatch(thunkGetAllPracticeSessions(user.id));
+  };
+  const handleInstrumentClick = async e => {
+    setContentView('Instruments');
+
+    dispatch(thunkGetAllInstruments(user.id));
+  };
+  const handleGoalClick = async e => {
+    setContentView('Goals');
+
+    dispatch(thunkGetAllGoals(user.id));
+  };
 
   useEffect(() => {
     if (!showSideBar) return;
@@ -38,6 +55,15 @@ const UserDashboard = () => {
     return () => document.removeEventListener('click', closeMenu);
   }, [showSideBar]);
 
+  let contentCard;
+
+  if (contentView === 'Practice Sessions') contentCard = <PracticeSessionCard />;
+  if (contentView === 'Instruments') contentCard = <InstrumentCard />;
+  if (contentView === 'Goals') contentCard = <GoalCard />;
+
+  logger('goals', goals);
+  logger('instruments', instruments);
+
   return (
     <>
       <header className="user-dashboard__header">
@@ -46,12 +72,39 @@ const UserDashboard = () => {
           <ProfileButton user={user} />
         </div>
       </header>
-      <main>
-        <aside
-          className={`user-dashboard__sidebar ${showSideBar ? 'show' : ''}`}
-          ref={sidebar}
-        ></aside>
-        {showSideBar && <div className="overlay"></div>}
+      <aside
+        className={`user-dashboard__sidebar ${showSideBar ? 'show' : ''}`}
+        ref={sidebar}
+      ></aside>
+      {showSideBar && <div className="overlay"></div>}
+
+      <main className="user-dashboard__main-content">
+        <section className="user-dashboard__user-info-section">
+          <UserInfoCard user={user} />
+        </section>
+
+        <section className="user-dashboard__view-selection-section">
+          <div className="user-dashboard__view-selection">
+            <button onClick={handleInstrumentClick}>Instruments</button>
+            <button onClick={handlePracticeSessionClick}>Practice Sessions</button>
+            <button onClick={handleGoalClick}>Goals</button>
+          </div>
+        </section>
+
+        <section className="user-dashboard__content-section">
+          <div className="user-dashboard__content-container">
+            {contentView === 'Practice Sessions' &&
+              practiceSessions.map(session => (
+                <PracticeSessionCard key={session.id} session={session} />
+              ))}
+            {contentView === 'Instruments' &&
+              instruments.map(instrument => (
+                <InstrumentCard key={instrument.id} instrument={instrument} />
+              ))}
+            {contentView === 'Goals' &&
+              goals.map(goal => <GoalCard key={goal.id} goal={goal} />)}
+          </div>
+        </section>
       </main>
     </>
   );
