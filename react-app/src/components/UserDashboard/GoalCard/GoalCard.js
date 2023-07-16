446b6a5dc1e80db2026c02a11c95f644b1a5464a
Christian Spada <spada17@gmail.com>
2023-07-15 22:46:38 -0400

    work on goal card

---
+++ react-app/src/components/UserDashboard/GoalCard/GoalCard.js
@@ -0,0 +1,11 @@
+import './GoalCard.css';
+
+const GoalCard = ({ goal }) => {
+  return (
+    <div style={{ height: '200px', width: '200px', backgroundColor: 'lightblue' }}>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default GoalCard;
