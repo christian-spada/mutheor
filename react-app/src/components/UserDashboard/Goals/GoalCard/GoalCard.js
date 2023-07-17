07aee8031cac5eadf93eeb6a31f13e6d7b1bb872
Christian Spada <spada17@gmail.com>
2023-07-17 16:52:28 -0400

    Work on goal card

--- react-app/src/components/UserDashboard/Goals/GoalCard/GoalCard.js
+++ react-app/src/components/UserDashboard/Goals/GoalCard/GoalCard.js
@@ -1,12 +1,34 @@
+import { formatDate } from '../../../../utils/helpers';
 import './GoalCard.css';
 
-const GoalCard = ({ goal }) => {
+const GoalCard = ({ goal, user }) => {
   return (
     <div className="goal-card card">
       <header className="goal-card__header">
         <i className="fa-regular fa-pen-to-square"></i>
         <i className="fa-solid fa-xmark"></i>
       </header>
+      <section className="goal-card__info-section">
+        <div>
+          <i className="fa-solid fa-sliders"></i>
+          <span>
+            {goal.instrument.type}{' '}
+            {goal.instrument.nickname !== 'Needs nickname!' ? goal.instrument.nickname : ''}
+          </span>
+        </div>
+        <div>
+          <i className="fa-regular fa-calendar"></i>
+          <time>{formatDate(goal.targetDate, '-')}</time>
+        </div>
+      </section>
+
+      <section className="goal-card__days-remaining-section">
+        <p className="goal-card__days-remaining">25 days remaining</p>
+      </section>
+
+      <section className="goal-card__description-section">
+        <p className="goal-card__description">{goal.description}</p>
+      </section>
     </div>
   );
 };
