38345247059e2cf07fb51ee7227314ad5e931bca
Christian Spada <spada17@gmail.com>
2023-07-17 17:28:57 -0400

    finish delete goal functionality

---
+++ react-app/src/components/UserDashboard/Goals/DeleteGoalModal/DeleteGoalModal.js
@@ -0,0 +1,38 @@
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../../../context/Modal';
+import './DeleteGoalModal.css';
+import { thunkDeleteGoal } from '../../../../store/goals';
+import { logger } from '../../../../utils/helpers';
+
+export const DeleteGoalModal = ({ goalToDelete, user }) => {
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
+
+  const handleDelete = async () => {
+    const message = await dispatch(thunkDeleteGoal(user.id, goalToDelete));
+    logger('message res', message);
+    closeModal();
+  };
+
+  return (
+    <div className="delete-goal-modal">
+      <p>Are you sure you want to delete this goal? This action cannot be undone.</p>
+      <div className="delete-goal-modal__btn-container">
+        <button
+          className="delete-goal-modal__cancel-btn"
+          onClick={closeModal}
+        >
+          Cancel
+        </button>
+        <button
+          className="delete-goal-modal__delete-btn"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteGoalModal;
