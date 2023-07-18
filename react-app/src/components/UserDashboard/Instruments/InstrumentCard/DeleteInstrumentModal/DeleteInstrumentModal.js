9a6340f1ed1cd319cfade89c72b84aa5a103e18d
Christian Spada <spada17@gmail.com>
2023-07-17 20:09:03 -0400

    add delete instrument functionality

--- react-app/src/components/UserDashboard/Instruments/InstrumentCard/DeleteInstrumentModal/DeleteInstrumentModal.js
+++ react-app/src/components/UserDashboard/Instruments/InstrumentCard/DeleteInstrumentModal/DeleteInstrumentModal.js
@@ -3,14 +3,15 @@ import { useDispatch } from 'react-redux';
 import './DeleteInstrumentModal.css';
 import { useModal } from '../../../../../context/Modal';
 import { logger } from '../../../../../utils/helpers';
+import { thunkGetUser } from '../../../../../store/session';
 
 const DeleteInstrumentModal = ({ instrumentToDelete, user }) => {
   const dispatch = useDispatch();
   const { closeModal } = useModal();
 
   const handleDelete = async () => {
-    const message = await dispatch(thunkDeleteInstrument(user.id, instrumentToDelete));
-    logger('message res', message);
+    await dispatch(thunkDeleteInstrument(user.id, instrumentToDelete));
+    await dispatch(thunkGetUser(user.id));
     closeModal();
   };
 
@@ -18,10 +19,16 @@ const DeleteInstrumentModal = ({ instrumentToDelete, user }) => {
     <div className="delete-instrument-modal">
       <p>Are you sure you want to delete this instrument? This action cannot be undone.</p>
       <div className="delete-instrument-modal__btn-container">
-        <button className="delete-instrument-modal__cancel-btn" onClick={closeModal}>
+        <button
+          className="delete-instrument-modal__cancel-btn"
+          onClick={closeModal}
+        >
           Cancel
         </button>
-        <button className="delete-instrument-modal__delete-btn" onClick={handleDelete}>
+        <button
+          className="delete-instrument-modal__delete-btn"
+          onClick={handleDelete}
+        >
           Delete
         </button>
       </div>
