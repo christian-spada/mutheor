98d537c062c5523a64c936c33bda3a93249ef8e7
Christian Spada <spada17@gmail.com>
2023-07-18 20:04:20 -0400

    refactor image field on edit instrument form to handle s3 uploads

--- app/forms/instruments/edit_instrument_form.py
+++ app/forms/instruments/edit_instrument_form.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField
 from wtforms.validators import DataRequired, ValidationError
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from app.api.aws_helpers import ALLOWED_EXTENSIONS
 
 type_choices = [
     ("Electric Guitar"),
@@ -22,24 +24,16 @@ type_choices = [
 category_choices = [("Percussion"), ("Brass"), ("Woodwind"), ("String"), ("Electronic")]
 
 
-def is_image_url(form, field):
-    image = field.data
-    if not image:
-        return
-
-    image_name = image.split("/")[-1]
-    split = image_name.split(".")
-    valid_extensions = ["jpg", "jpeg", "png"]
-    if (not len(split) == 2) or (split[1] not in valid_extensions):
-        raise ValidationError(
-            "Image url must be of type " + ", ".join(valid_extensions)
-        )
-
-
 class EditInstrumentForm(FlaskForm):
     type = SelectField("type", choices=type_choices, validators=[DataRequired()])
     model = StringField("model", validators=[DataRequired()])
     category = SelectField(
         "category", choices=category_choices, validators=[DataRequired()]
     )
-    image = StringField("image", validators=[DataRequired(), is_image_url])
+    image = FileField(
+        "image",
+        validators=[
+            FileRequired(),
+            FileAllowed(list(ALLOWED_EXTENSIONS)),
+        ],
+    )
