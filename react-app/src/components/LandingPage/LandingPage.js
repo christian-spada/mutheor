60c31c34d059ee40063056bd1434c06c30d5e611
Christian Spada <spada17@gmail.com>
2023-07-21 20:10:02 -0400

    work with intersection observer on landing page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,53 @@
 import { NavBar } from './NavBar/NavBar';
 import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import './LandingPage.css';
-import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { logger } from '../../utils/helpers';
+import { useEffect, useRef, useState } from 'react';
+import './LandingPage.css';
 
 const LandingPage = () => {
-  const history = useHistory();
   const user = useSelector(state => state.session.user);
+  // const [isInstrumentsVisible, setIsInstrumentsVisible] = useState();
+  // const [isSessionsVisible, setIsSessionsVisible] = useState();
+  // const [isGoalsVisible, setIsGoalsVisible] = useState();
+  const instrumentsText = useRef();
+  const instrumentsImg = useRef();
+  const sessionsText = useRef();
+  const sessionsImg = useRef();
+  const goalsText = useRef();
+  const goalsImg = useRef();
+
+  const elementRefs = [
+    instrumentsText,
+    instrumentsImg,
+    sessionsText,
+    sessionsImg,
+    goalsText,
+    goalsImg,
+  ];
+
+  useEffect(() => {
+    const handleIntersect = entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && entry.target.tagName === 'DIV') {
+          entry.target.classList.add('show-text');
+        }
+        if (entry.isIntersecting && entry.target.tagName === 'IMG') {
+          entry.target.classList.add('show-img');
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersect);
+
+    elementRefs.forEach(ref => {
+      observer.observe(ref.current);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
     <div className="landing-page">
@@ -34,7 +74,10 @@ const LandingPage = () => {
       </section>
       <section className="instruments-section">
         <div className="instruments-section__content-container">
-          <div>
+          <div
+            ref={instrumentsText}
+            className="fade"
+          >
             <h2 className="instruments__headline">
               Create records for all of your instruments
             </h2>
@@ -43,14 +86,19 @@ const LandingPage = () => {
             </h3>
           </div>
           <img
+            ref={instrumentsImg}
             src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Instrument-card-image.png"
             alt="instrument-card"
+            className="fade"
           ></img>
         </div>
       </section>
       <section className="sessions-section">
         <div className="sessions-section__content-container">
-          <div>
+          <div
+            ref={sessionsText}
+            className="fade"
+          >
             <h2 className="sessions__headline">
               Create records for all of your practice sessions. Upload your own pictures as
               well.
@@ -61,25 +109,33 @@ const LandingPage = () => {
             </h3>
           </div>
           <img
+            ref={sessionsImg}
             src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Session-Card-image.png"
             alt="session-card"
+            className="fade"
           ></img>
         </div>
       </section>
       <section className="goals-section">
         <div className="goals-section__content-container">
-          <div>
+          <div
+            ref={goalsText}
+            className="fade"
+          >
             <h2 className="goals__headline">
               Create records for all of your goals. Upload your own pictures as well.
             </h2>
             <h3 className="subheading">Set goals to keep yourself accountable</h3>
           </div>
           <img
+            ref={goalsImg}
             src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Goal-Card-image.png"
             alt="goal-card"
+            className="fade"
           ></img>
         </div>
       </section>
+      <footer style={{ height: '400px', backgroundColor: 'var(--primary-600)' }}></footer>
     </div>
   );
 };
