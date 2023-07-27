27b1b50a22c4d5d19e7b3943b92749c9a9f1d25c
Christian Spada <spada17@gmail.com>
2023-07-27 13:15:59 -0400

    styling on landing page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -47,72 +47,62 @@ const LandingPage = () => {
 
   return (
     <div className="landing-page">
-      <header style={{ position: 'sticky', top: 0 }}>
+      <header className="navbar-container">
         <NavBar user={user} />
       </header>
       <Hero />
       <section className="instruments-section">
-        <div className="instruments-section__content-container">
-          <div
-            ref={instrumentsText}
-            className="fade"
-          >
-            <h2 className="instruments__headline">
-              Create records for all of your instruments
-            </h2>
-            <h3 className="subheading">
-              Keep all of your instruments in one place with pictures and info on them
-            </h3>
-          </div>
-          <img
-            ref={instrumentsImg}
-            src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Instrument-card-image.png"
-            alt="instrument-card"
-            className="fade"
-          ></img>
+        <div
+          ref={instrumentsText}
+          className="fade"
+        >
+          <h2 className="instruments__headline">Create records for all of your instruments</h2>
+          <h3 className="subheading">
+            Keep all of your instruments in one place with pictures and info on them
+          </h3>
         </div>
+        <img
+          ref={instrumentsImg}
+          src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Instrument-card-image.png"
+          alt="instrument-card"
+          className="fade"
+        ></img>
       </section>
       <section className="sessions-section">
-        <div className="sessions-section__content-container">
-          <div
-            ref={sessionsText}
-            className="fade"
-          >
-            <h2 className="sessions__headline">
-              Create records for all of your practice sessions. Upload your own pictures as
-              well.
-            </h2>
-            <h3 className="subheading">
-              Keep record of all of your sessions with date information and notes on the
-              session
-            </h3>
-          </div>
-          <img
-            ref={sessionsImg}
-            src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Session-Card-image.png"
-            alt="session-card"
-            className="fade"
-          ></img>
+        <div
+          ref={sessionsText}
+          className="fade"
+        >
+          <h2 className="sessions__headline">
+            Create records for all of your practice sessions. Upload your own pictures as well.
+          </h2>
+          <h3 className="subheading">
+            Keep record of all of your sessions with date information and notes on the session
+          </h3>
         </div>
+        <img
+          ref={sessionsImg}
+          src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Session-Card-image.png"
+          alt="session-card"
+          className="fade"
+        ></img>
       </section>
       <section className="goals-section">
-        <div className="goals-section__content-container">
-          <div
-            ref={goalsText}
-            className="fade"
-          >
-            <h2 className="goals__headline">
-              Create records for all of your goals. Upload your own pictures as well.
-            </h2>
-            <h3 className="subheading">Set goals to keep yourself accountable</h3>
-          </div>
-          <img
-            ref={goalsImg}
-            src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Goal-Card-image.png"
-            alt="goal-card"
-            className="fade"
-          ></img>
+        <div
+          ref={goalsText}
+          className="fade"
+        >
+          <h2 className="goals__headline">
+            Create records for all of your goals. Upload your own pictures as well.
+          </h2>
+          <h3 className="subheading">Set goals to keep yourself accountable</h3>
         </div>
+        <img
+          ref={goalsImg}
+          src="https://my-mutheor-user-images-bucket.s3.amazonaws.com/Goal-Card-image.png"
+          alt="goal-card"
+          className="fade"
+        ></img>
       </section>
       <footer className="landing-page__footer"></footer>
     </div>
