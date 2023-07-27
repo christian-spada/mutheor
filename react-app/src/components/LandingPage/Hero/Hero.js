a6a17204d1ff7a0696c3de1360be87d99baa2507
Christian Spada <spada17@gmail.com>
2023-07-27 16:37:05 -0400

    make navbar fixed position

--- react-app/src/components/LandingPage/Hero/Hero.js
+++ react-app/src/components/LandingPage/Hero/Hero.js
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import './Hero.css';
+import NavBar from '../NavBar/NavBar';
 
-const Hero = () => {
+const Hero = ({ user, heroRef }) => {
   return (
-    <section className="hero">
-      <div className="hero__heading">
-        <h1 className="hero__headline">Level Up Your Practice</h1>
-        <h2 className="hero__subheading">
-          Mutheor is a place for musicians to track their progress with practice session data
-          and goal setting
-        </h2>
-        <Link to="/signup">
-          <button className="hero__action-btn">Start Your Journey</button>
-        </Link>
+    <section
+      ref={heroRef}
+      className="hero"
+    >
+      <NavBar user={user} />
+      <div className="hero__layout">
+        <div className="hero__heading">
+          <h1 className="hero__headline">Level Up Your Practice</h1>
+          <h2 className="hero__subheading">
+            Mutheor is a place for musicians to track their progress with practice session data
+            and goal setting
+          </h2>
+          <Link to="/signup">
+            <button className="hero__action-btn">Start Your Journey</button>
+          </Link>
+        </div>
       </div>
     </section>
   );
