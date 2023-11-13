f07eeae55ccf42005d89d2184333dffa7ee37031
Christian Spada <spada17@gmail.com>
2023-11-13 11:37:09 -0500

    add card flip funtionality to instruments

--- react-app/src/components/UserDashboard/Instruments/InstrumentCard/InstrumentCard.js
+++ react-app/src/components/UserDashboard/Instruments/InstrumentCard/InstrumentCard.js
@@ -1,11 +1,17 @@
 import OpenModalButton from '../../../OpenModalButton';
 import DeleteInstrumentModal from './DeleteInstrumentModal/DeleteInstrumentModal';
 import EditInstrumentModal from './EditInstrumentModal/EditInstrumentModal';
+import useCardFlip from '../../../../custom-hooks/useCardFlip';
 import './InstrumentCard.css';
 
 const InstrumentCard = ({ instrument, user }) => {
+  const { isCardFlipped, flipCard } = useCardFlip();
+
   return (
-    <div className="instrument-card card">
+    <div
+      className={`instrument-card card ${isCardFlipped ? 'isFlipped' : ''}`}
+      onClick={flipCard}
+    >
       <header className="instrument-card__header">
         <OpenModalButton
           modalComponent={
@@ -27,7 +33,7 @@ const InstrumentCard = ({ instrument, user }) => {
           icon={<i className="fa-solid fa-xmark instrument-card__delete-btn"></i>}
         />
       </header>
-      <section className="instrument-card__img-section">
+      <section className={`instrument-card__img-section ${isCardFlipped ? 'isFlipped' : ''}`}>
         <div className="instrument-card__img-container">
           <img
             src={instrument.image}
@@ -36,24 +42,26 @@ const InstrumentCard = ({ instrument, user }) => {
           ></img>
         </div>
       </section>
-      <section className="instrument-card__model-section">
-        <div>
-          <i className="fa-solid fa-signature"></i>
-          <p className="instrument-card__model">{instrument.model}</p>
-        </div>
-      </section>
-      <section className="instrument-card__type-section">
-        <div>
-          <i className="fa-solid fa-sliders"></i>
-          <p>{instrument.type}</p>
-        </div>
-      </section>
-      <section className="instrument-card__category-section">
-        <div>
-          <i className="fa-solid fa-circle-info"></i>
-          <p>{instrument.category}</p>
-        </div>
-      </section>
+      <div className={`instrument-card__info-container ${isCardFlipped ? 'isFlipped' : ''}`}>
+        <section className="instrument-card__model-section">
+          <div>
+            <i className="fa-solid fa-signature"></i>
+            <p className="instrument-card__model">{instrument.model}</p>
+          </div>
+        </section>
+        <section className="instrument-card__type-section">
+          <div>
+            <i className="fa-solid fa-sliders"></i>
+            <p>{instrument.type}</p>
+          </div>
+        </section>
+        <section className="instrument-card__category-section">
+          <div>
+            <i className="fa-solid fa-circle-info"></i>
+            <p>{instrument.category}</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
