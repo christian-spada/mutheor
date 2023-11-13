ec23ec8c773a73306bcfb3d30f2ed78eecda1e0c
Christian Spada <spada17@gmail.com>
2023-11-13 11:37:20 -0500

    add card flip funtionality to practice sessions

--- react-app/src/components/UserDashboard/PracticeSessions/PracticeSessionCard/PracticeSessionCard.js
+++ react-app/src/components/UserDashboard/PracticeSessions/PracticeSessionCard/PracticeSessionCard.js
@@ -1,33 +1,40 @@
 import { formatDate, formatDuration } from '../../../../utils/helpers';
+import useCardFlip from '../../../../custom-hooks/useCardFlip';
 import './PracticeSessionCard.css';
 
 const PracticeSessionCard = ({ session }) => {
+  const { isCardFlipped, flipCard } = useCardFlip();
   return (
-    <div className="session-card card">
-      <section className="session-card__time-section">
-        <div>
-          <i className="fa-regular fa-calendar"></i>
-          <time>{formatDate(session.date, '-')}</time>
-        </div>
-        <div>
-          <i className="fa-regular fa-clock"></i>
-          <span>{formatDuration(session.duration)}</span>
-        </div>
-      </section>
-      <section className="session-card__info-section">
-        <div>
-          <i className="fa-solid fa-sliders"></i>
+    <div
+      className={`session-card card ${isCardFlipped ? 'isFlipped' : ''}`}
+      onClick={flipCard}
+    >
+      <div className={`session-card__info-container ${isCardFlipped ? 'isFlipped' : ''}`}>
+        <section className="session-card__time-section">
           <div>
-            <p className="session-card__model">{session.instrument.model}</p>
-            <p className="session-card__type">{session.instrument.type}</p>
+            <i className="fa-regular fa-calendar"></i>
+            <time>{formatDate(session.date, '-')}</time>
           </div>
-        </div>
-        <div>
-          <i className="fa-solid fa-arrows-to-circle"></i>
-          <span className="session-card__focus">{session.areaOfFocus}</span>
-        </div>
-      </section>
-      <section className="session-card__notes-section">
+          <div>
+            <i className="fa-regular fa-clock"></i>
+            <span>{formatDuration(session.duration)}</span>
+          </div>
+        </section>
+        <section className="session-card__info-section">
+          <div>
+            <i className="fa-solid fa-sliders"></i>
+            <div>
+              <p className="session-card__model">{session.instrument.model}</p>
+              <p className="session-card__type">{session.instrument.type}</p>
+            </div>
+          </div>
+          <div>
+            <i className="fa-solid fa-arrows-to-circle"></i>
+            <span className="session-card__focus">{session.areaOfFocus}</span>
+          </div>
+        </section>
+      </div>
+      <section className={`session-card__notes-section ${isCardFlipped ? 'isFlipped' : ''}`}>
         <p>{session.notes}</p>
       </section>
     </div>
