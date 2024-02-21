fb76cdca5871fc6dfea861ebf013d58417a588f1
Christian Spada <spada17@gmail.com>
2024-02-21 17:56:24 -0500

    Remove unused logger function

--- react-app/src/components/UserDashboard/Goals/DeleteGoalModal/DeleteGoalModal.js
+++ react-app/src/components/UserDashboard/Goals/DeleteGoalModal/DeleteGoalModal.js
@@ -2,7 +2,6 @@ import { useDispatch } from 'react-redux';
 import { useModal } from '../../../../context/Modal';
 import './DeleteGoalModal.css';
 import { thunkDeleteGoal } from '../../../../store/goals';
-import { logger } from '../../../../utils/helpers';
 import { thunkGetUser } from '../../../../store/session';
 
 export const DeleteGoalModal = ({ goalToDelete, user }) => {
