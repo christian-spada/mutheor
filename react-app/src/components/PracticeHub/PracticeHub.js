4bab2d08da3b1e15037e6265b37afd41eac923d6
Christian Spada <spada17@gmail.com>
2024-05-31 14:37:35 -0400

    add sidebar and header component to practice hub page

--- react-app/src/components/PracticeHub/PracticeHub.js
+++ react-app/src/components/PracticeHub/PracticeHub.js
@@ -1,10 +1,32 @@
+import { useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Metronome from './Metronome/Metronome';
 import SessionTimer from './SessionTimer/SessionTimer';
+import Sidebar from '../UserDashboard/Sidebar/Sidebar';
+import Header from '../Header/Header';
+import useCloseSideBar from '../../custom-hooks/useCloseSideBar';
 import './PracticeHub.css';
 
 const PracticeHub = () => {
+  const user = useSelector(state => state.session.user);
+  const [showSideBar, setShowSidebar] = useState();
+  const sidebarRef = useRef();
+
+  useCloseSideBar(sidebarRef, showSideBar, setShowSidebar);
+
   return (
     <>
+      <Header
+        showSideBar={showSideBar}
+        setShowSidebar={setShowSidebar}
+        user={user}
+      />
+      <Sidebar
+        sidebarRef={sidebarRef}
+        showSideBar={showSideBar}
+        user={user}
+        isPracticeHub={true}
+      />
       <section className="practice-hub">
         <SessionTimer />
         <Metronome />
