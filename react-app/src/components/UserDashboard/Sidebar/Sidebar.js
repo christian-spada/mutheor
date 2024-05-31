86f2f326754f2cdb46a9e1ef678f8ac1f4769ac5
Christian Spada <spada17@gmail.com>
2024-05-31 14:20:13 -0400

    add overlay logic to sidebar component to improve reusability

--- react-app/src/components/UserDashboard/Sidebar/Sidebar.js
+++ react-app/src/components/UserDashboard/Sidebar/Sidebar.js
@@ -3,25 +3,28 @@ import './Sidebar.css';
 
 const Sidebar = ({ sidebarRef, showSideBar, user }) => {
   return (
-    <aside
-      className={`sidebar ${showSideBar ? 'show' : ''}`}
-      ref={sidebarRef}
-    >
-      <ul className="sidebar__link-container">
-        <Link to="/">
-          <li className="sidebar__link">Home</li>
-        </Link>
-        <Link to={`/users/${user.id}/practice-hub`}>
-          <li className="sidebar__link">Practice Hub</li>
-        </Link>
-        <li
-          className="sidebar__link"
-          onClick={() => alert('Feature coming soon!')}
-        >
-          Your Stats
-        </li>
-      </ul>
-    </aside>
+    <>
+      <aside
+        className={`sidebar ${showSideBar ? 'show' : ''}`}
+        ref={sidebarRef}
+      >
+        <ul className="sidebar__link-container">
+          <Link to="/">
+            <li className="sidebar__link">Home</li>
+          </Link>
+          <Link to={`/users/${user.id}/practice-hub`}>
+            <li className="sidebar__link">Practice Hub</li>
+          </Link>
+          <li
+            className="sidebar__link"
+            onClick={() => alert('Feature coming soon!')}
+          >
+            Your Stats
+          </li>
+        </ul>
+      </aside>
+      {showSideBar && <div className="overlay"></div>}
+    </>
   );
 };
 
