690bf1cd29c406d371e55b7cd837ad4dc498e3c0
Christian Spada <spada17@gmail.com>
2024-05-31 14:37:11 -0400

    add props to dynamically render sidebar list based on which page sidebar is mounted on

--- react-app/src/components/UserDashboard/Sidebar/Sidebar.js
+++ react-app/src/components/UserDashboard/Sidebar/Sidebar.js
@@ -1,27 +1,70 @@
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
-const Sidebar = ({ sidebarRef, showSideBar, user }) => {
+const Sidebar = ({
+  sidebarRef,
+  showSideBar,
+  user,
+  isDashboard,
+  isPracticeHub,
+  isYourStats,
+}) => {
+  const fromDashboard = (
+    <ul className="sidebar__link-container">
+      <Link to="/">
+        <li className="sidebar__link">Home</li>
+      </Link>
+      <Link to={`/users/${user.id}/practice-hub`}>
+        <li className="sidebar__link">Practice Hub</li>
+      </Link>
+      <li
+        className="sidebar__link"
+        onClick={() => alert('Feature coming soon!')}
+      >
+        Your Stats
+      </li>
+    </ul>
+  );
+
+  const fromPracticeHub = (
+    <ul className="sidebar__link-container">
+      <Link to="/">
+        <li className="sidebar__link">Home</li>
+      </Link>
+      <Link to={`/users/${user.id}/dashboard`}>
+        <li className="sidebar__link">Dashboard</li>
+      </Link>
+      <li
+        className="sidebar__link"
+        onClick={() => alert('Feature coming soon!')}
+      >
+        Your Stats
+      </li>
+    </ul>
+  );
+
+  const fromYourStats = (
+    <ul className="sidebar__link-container">
+      <Link to="/">
+        <li className="sidebar__link">Home</li>
+      </Link>
+      <Link to={`/users/${user.id}/dashboard`}>
+        <li className="sidebar__link">Dashboard</li>
+      </Link>
+      <Link to={`/users/${user.id}/practice-hub`}>
+        <li className="sidebar__link">Practice Hub</li>
+      </Link>
+    </ul>
+  );
   return (
     <>
       <aside
         className={`sidebar ${showSideBar ? 'show' : ''}`}
         ref={sidebarRef}
       >
-        <ul className="sidebar__link-container">
-          <Link to="/">
-            <li className="sidebar__link">Home</li>
-          </Link>
-          <Link to={`/users/${user.id}/practice-hub`}>
-            <li className="sidebar__link">Practice Hub</li>
-          </Link>
-          <li
-            className="sidebar__link"
-            onClick={() => alert('Feature coming soon!')}
-          >
-            Your Stats
-          </li>
-        </ul>
+        {isDashboard && fromDashboard}
+        {isPracticeHub && fromPracticeHub}
+        {isYourStats && fromYourStats}
       </aside>
       {showSideBar && <div className="overlay"></div>}
     </>
